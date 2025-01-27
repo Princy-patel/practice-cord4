ed94b271047c59018b114a6fb7087f0fdb150986
Princy Patel <kapatelprincy@gmail.com>
2025-01-27 19:56:30 +0530

    fix:persist state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slice/authSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    auth: userReducer,
+    auth: persistedReducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
