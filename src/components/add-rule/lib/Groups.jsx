07016f4c9df37258f48d2e5e3048dcbef350775d
Princy Patel <kapatelprincy@gmail.com>
2025-02-18 23:10:52 +0530

    fix:change the graph data according to radio

--- src/components/add-rule/lib/Groups.jsx
+++ src/components/add-rule/lib/Groups.jsx
@@ -1,4 +1,4 @@
-import { Select } from "antd";
+import { Radio, Select } from "antd";
 import { useState } from "react";
 import Chart from "react-apexcharts";
 
@@ -22,22 +22,22 @@ function Groups() {
   const graphSeries = [
     {
       name: "TEAM A",
-      type: "area",
+      type: "bar",
       data: [22, 33, 15, 23, 18, 21, 13, 20, 15, 24, 17],
     },
     {
       name: "TEAM B",
-      type: "area",
+      type: "bar",
       data: [38, 45, 28, 42, 25, 36, 21, 39, 27, 46, 29],
     },
     {
       name: "TEAM C",
-      type: "area",
+      type: "bar",
       data: [95, 110, 85, 105, 70, 190, 150, 175, 130, 135, 65],
     },
     {
       name: "TEAM D",
-      type: "area",
+      type: "bar",
       data: [110, 125, 90, 120, 80, 210, 160, 190, 140, 145, 75],
     },
     {
@@ -47,7 +47,7 @@ function Groups() {
     },
     {
       name: "TEAM F",
-      type: "area",
+      type: "bar",
       data: [80, 95, 70, 90, 60, 170, 130, 155, 110, 115, 55],
     },
     {
@@ -57,22 +57,22 @@ function Groups() {
     },
     {
       name: "TEAM H",
-      type: "area",
+      type: "bar",
       data: [60, 75, 50, 70, 45, 65, 40, 60, 48, 72, 50],
     },
     {
       name: "TEAM I",
-      type: "area",
+      type: "bar",
       data: [70, 85, 60, 80, 55, 75, 50, 70, 58, 82, 60],
     },
     {
       name: "TEAM J",
-      type: "area",
+      type: "bar",
       data: [50, 65, 40, 60, 35, 55, 30, 50, 38, 62, 40],
     },
     {
       name: "TEAM K",
-      type: "area",
+      type: "bar",
       data: [90, 105, 80, 100, 75, 180, 140, 165, 120, 125, 60],
     },
   ];
@@ -80,7 +80,7 @@ function Groups() {
   const filteredSeries = graphSeries.filter((item) =>
     selectedData.includes(item.name)
   );
-  
+
   const option = {
     series: filteredSeries,
     chart: {
@@ -111,14 +111,31 @@ function Groups() {
     },
   };
 
-  console.log(filteredSeries);
 
-  const handleChange = (value) => {
-    console.log(`selected ${value}`);
+  const onRadioChange = (e) => {
+    const teamMapping = {
+      team_a: "TEAM A",
+      team_k: "TEAM K",
+    };
+    // setSelectedTeam(teamMapping[e.target.value]);
+    setSelectedData(teamMapping[e.target.value]);
   };
+
+  const options = [
+    { label: "TEAM A", value: "team_a" },
+    { label: "TEAM k", value: "team_k" },
+  ];
 
   return (
     <div className="h-screen w--screen">
+      <Radio.Group
+        block
+        options={options}
+        onChange={onRadioChange}
+        defaultValue="team_a"
+        optionType="button"
+        buttonStyle="solid"
+      />
       <Select
         mode="multiple"
         style={{ width: "100%" }}
